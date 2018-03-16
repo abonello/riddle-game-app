0867d840595906527e8a5a45ef33dda81cbd03f6
Anthony Bonello <ab@anthonybonello.co.uk>
2018-03-16 01:20:50 +0000

    Added templates for About, Contact and Hall of Fame. Manually tested their routing. Working well.

--- run.py
+++ run.py
@@ -10,14 +10,17 @@ def index():
     
 @app.route('/halloffame')
 def halloffame():
-    return "<h1>Riddle-Me-This Application</h1><h2>Hall of Fame</h2>"
+    # return "<h1>Riddle-Me-This Application</h1><h2>Hall of Fame</h2>"
+    return render_template("halloffame.html")
     
 @app.route('/about')
 def about():
-    return "<h1>Riddle-Me-This Application</h1><h2>About</h2>"
+    # return "<h1>Riddle-Me-This Application</h1><h2>About</h2>"
+    return render_template("about.html")
     
 @app.route('/contact')
 def contact():
-    return "<h1>Riddle-Me-This Application</h1><h2>Contact</h2>"
+    # return "<h1>Riddle-Me-This Application</h1><h2>Contact</h2>"
+    return render_template("contact.html")
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
